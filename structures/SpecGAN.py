37e76ab4cadb319f28b0114fc405411830c08d19
Daniel Braithwaite <dbraithwaite@google.com>
2020-07-03 00:40:02 +0000

    Update SpecGAN Structure to Have Correct Filters

--- structures/SpecGAN.py
+++ structures/SpecGAN.py
@@ -14,47 +14,50 @@
 
 import tensorflow as tf
 from tensorflow.keras.layers import Dense, ReLU, LeakyReLU, Conv2D, Conv2DTranspose, Reshape, AveragePooling1D, Flatten
+from tensorflow.keras.activations import linear
 from tensorflow.keras import Model, Sequential
 
 class Generator(Model):
-    def __init__(self, channels=1, d=4):
+    def __init__(self, channels=1, d=4, activation=linear):
         super(Generator, self).__init__()
         
+        self.activation = activation
+        
         layers = []
-        layers.append(Dense(256*d))
-        layers.append(Reshape((4, 4, 16*d)))
+        layers.append(Dense(4 * 8 * 1024))
+        layers.append(Reshape((4, 8, 1024)))
         layers.append(ReLU())
-        layers.append(Conv2DTranspose(filters=8*d, kernel_size=(6,6), strides=(2,2), padding='same'))
+        layers.append(Conv2DTranspose(filters=512, kernel_size=(6,6), strides=(2,2), padding='same'))
         layers.append(ReLU())
-        layers.append(Conv2DTranspose(filters=4*d, kernel_size=(6,6), strides=(2,2), padding='same'))
+        layers.append(Conv2DTranspose(filters=256, kernel_size=(6,6), strides=(2,2), padding='same'))
         layers.append(ReLU())
-        layers.append(Conv2DTranspose(filters=2*d, kernel_size=(6,6), strides=(2,2), padding='same'))
+        layers.append(Conv2DTranspose(filters=128, kernel_size=(6,6), strides=(2,2), padding='same'))
         layers.append(ReLU())
-        layers.append(Conv2DTranspose(filters=1*d, kernel_size=(6,6), strides=(2,2), padding='same'))
+        layers.append(Conv2DTranspose(filters=64, kernel_size=(6,6), strides=(2,2), padding='same'))
         layers.append(ReLU())
         layers.append(Conv2DTranspose(filters=channels, kernel_size=(6,6), strides=(2,2), padding='same'))
-        layers.append(ReLU())
-        layers.append(Conv2DTranspose(filters=channels, kernel_size=(6,6), strides=(1,2), padding='same'))
+        #layers.append(ReLU())
+        #layers.append(Conv2DTranspose(filters=channels, kernel_size=(6,6), strides=(1,2), padding='same'))
         
         self.l = Sequential(layers)
         
     def call(self, z):
-        return self.l(z)
+        return self.activation(self.l(z))
         
 class Discriminator(Model):
     def __init__(self, d=4):
         super(Discriminator, self).__init__()
         
         layers = []
-        layers.append(Conv2D(filters=d, kernel_size=(6,6), strides=(2,2), padding='same'))
+        layers.append(Conv2D(filters=64, kernel_size=(6,6), strides=(2,2), padding='same'))
         layers.append(LeakyReLU(alpha=0.2))
-        layers.append(Conv2D(filters=2*d, kernel_size=(6,6), strides=(2,2), padding='same'))
+        layers.append(Conv2D(filters=128, kernel_size=(6,6), strides=(2,2), padding='same'))
         layers.append(LeakyReLU(alpha=0.2))
-        layers.append(Conv2D(filters=4*d, kernel_size=(6,6), strides=(2,2), padding='same'))
+        layers.append(Conv2D(filters=256, kernel_size=(6,6), strides=(2,2), padding='same'))
         layers.append(LeakyReLU(alpha=0.2))
-        layers.append(Conv2D(filters=8*d, kernel_size=(6,6), strides=(2,2), padding='same'))
+        layers.append(Conv2D(filters=512, kernel_size=(6,6), strides=(2,2), padding='same'))
         layers.append(LeakyReLU(alpha=0.2))
-        layers.append(Conv2D(filters=16*d, kernel_size=(6,6), strides=(1,2), padding='same'))
+        layers.append(Conv2D(filters=1024, kernel_size=(6,6), strides=(1,2), padding='same'))
         layers.append(LeakyReLU(alpha=0.2))
         layers.append(Flatten())
         layers.append(Dense(1))
