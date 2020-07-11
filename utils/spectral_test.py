8e19a6680fcffaabfefed8aae54c6b3efeb384ac
Michael Chinen <mchinen@google.com>
2020-07-10 17:17:41 -0700

    Barebones bazel setup with dummy test

---
+++ utils/spectral_test.py
@@ -0,0 +1,20 @@
+# Lint as: python3
+"""Tests for spectral."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+import spectral
+
+
+class SpectralTest(tf.test.TestCase):
+
+  def test_give_me_a_name(self):
+    pass
+
+
+if __name__ == '__main__':
+  googletest.main()
