066533f87dacf38164b057d9a59f21fe3d446a26
Daniel Braithwaite <dbraithwaite@google.com>
2020-07-14 23:04:47 +0000

    Refactor WaveSpecGAN Training Script

--- train_maestro_wave_spec_gan.py
+++ train_maestro_wave_spec_gan.py
@@ -19,9 +19,10 @@ import numpy as np
 from audio_synthesis.structures import wave_gan
 from audio_synthesis.structures import spec_gan
 from audio_synthesis.datasets import maestro_dataset
-from audio_synthesis.models.MultiDiscriminatorWGAN import WGAN
+from audio_synthesis.models import wgan
+from audio_synthesis.utils import maestro_save_helper as save_helper
 
-os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+os.environ["CUDA_VISIBLE_DEVICES"] = ''
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
 # Setup Paramaters
@@ -58,32 +59,6 @@ def _get_discriminator_input_representations(x):
 
     return (tf.reshape(x, (-1, 2**14, 1)), magnitude)
 
-def _compute_losses(discriminator, d_real, d_fake, interpolated):
-    wasserstein_distance_waveform = tf.reduce_mean(d_real) - tf.reduce_mean(d_fake)
-    
-    # Compute waveform GP
-    with tf.GradientTape() as t:
-        t.watch(interpolated)
-        d_interp = discriminator(interpolated, training=True)
-            
-    grad = t.gradient(d_interp, [interpolated])[0]
-    redu_axis = list(range(len(interpolated.shape)))[1:]
-    slopes = tf.sqrt(tf.reduce_sum(tf.square(grad), axis=redu_axis))
-    gp = tf.reduce_mean((slopes - 1.0) ** 2.0)
-    
-    # Compute and return losses
-    g_loss = tf.reduce_mean(d_fake)
-    d_loss = wasserstein_distance_waveform + 10.0 * gp
-        
-    return g_loss, d_loss
-
-def save_examples(epoch, real, generated):  
-    real_waveforms = np.reshape(real, (-1))
-    gen_waveforms = np.reshape(generated, (-1))
-
-    sf.write(RESULT_DIR + 'real_' + str(epoch) + '.wav', real_waveforms, 16000)
-    sf.write(RESULT_DIR + 'gen_' + str(epoch) + '.wav', gen_waveforms, 16000)
-
 def main():
     raw_maestro = maestro_dataset.get_maestro_waveform_dataset(MAESTRO_PATH)
 
@@ -94,14 +69,20 @@ def main():
     generator_optimizer = tf.keras.optimizers.Adam(1e-4, beta_1=0.5, beta_2=0.9)
     discriminator_optimizer = tf.keras.optimizers.Adam(1e-4, beta_1=0.5, beta_2=0.9)
     
+    get_waveform = lambda waveform: waveform
+
+    save_examples = lambda epoch, real, generated:\
+        save_helper.save_wav_data(
+            epoch, real, generated, SAMPLING_RATE, RESULT_DIR, get_waveform
+        )
     
-    wave_gan_model = WGAN(
-        raw_maestro, [[-1, 2**14], [-1, 128, 256]], [[-1, 2**14, 1], [-1, 128, 256, 1]],
+    wave_gan_model = wgan.WGAN(
+        raw_maestro, [[-1, 2**14, 1], [-1, 128, 256, 1]],
         generator, [discriminator, spec_discriminator], Z_DIM, generator_optimizer,
-        discriminator_optimizer, generator_training_ratio=D_UPDATES_PER_G, batch_size=BATCH_SIZE,
-        epochs=EPOCHS, checkpoint_dir=CHECKPOINT_DIR, fn_compute_loss=_compute_losses,
+        discriminator_optimizer, discriminator_training_ratio=D_UPDATES_PER_G, batch_size=BATCH_SIZE,
+        epochs=EPOCHS, lambdas=[1.0, 1.0/1000.0], checkpoint_dir=CHECKPOINT_DIR,
         fn_save_examples=save_examples,
-        get_discriminator_input_representations=_get_discriminator_input_representations)
+        fn_get_discriminator_input_representations=_get_discriminator_input_representations)
 
     wave_gan_model.train()
     
