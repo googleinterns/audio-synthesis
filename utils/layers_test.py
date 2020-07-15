d1614d3747f72c1f32e74afb6e4b98eb476c7266
Daniel Braithwaite <dbraithwaite@google.com>
2020-07-15 23:02:56 +0000

    Add Second Shape Test for Layers Util

--- utils/layers_test.py
+++ utils/layers_test.py
@@ -22,6 +22,15 @@ class LayersTest(tf.test.TestCase):
         
         outputs = conv_transpose(inputs)
         self.assertShapeEqual(inputs, outputs)
+        
+    def test_conv_transpose_shape_upscale(self):
+        inputs = np.random.normal(size=(10, 5, 2)).astype(np.float32)
+        conv_transpose = layers.Conv1DTranspose(
+            filters=2, kernel_size=1, strides=2
+        )
+        
+        outputs = conv_transpose(inputs)
+        self.assertEqual((10, 10, 2), outputs.shape)
 
 
 if __name__ == '__main__':
