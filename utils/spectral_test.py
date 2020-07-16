4bc95a4641f3372018dfc5ecd98ab54bf84e08d8
Daniel Braithwaite <dbraithwaite@google.com>
2020-07-16 00:34:21 +0000

    Start Writing Tests for spectral.py

--- utils/spectral_test.py
+++ utils/spectral_test.py
@@ -6,15 +6,17 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
+import os
 
 import spectral
 
 
 class SpectralTest(tf.test.TestCase):
 
-  def test_give_me_a_name(self):
-    pass
+    def test_give_me_a_name(self):
+        pass
 
 
 if __name__ == '__main__':
-  googletest.main()
+    os.environ["CUDA_VISIBLE_DEVICES"] = ''
+    tf.test.main()
