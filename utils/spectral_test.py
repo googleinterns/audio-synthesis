f42c2a8599376e55a733095a34bc3899e9e9813d
Daniel Braithwaite <dbraithwaite@google.com>
2020-07-20 00:59:24 +0000

    Work on utils/spectral Tests

--- utils/spectral_test.py
+++ utils/spectral_test.py
@@ -6,6 +6,7 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
+import numpy as np
 import os
 
 import spectral
@@ -13,8 +14,38 @@ import spectral
 
 class SpectralTest(tf.test.TestCase):
 
-    def test_give_me_a_name(self):
-        pass
+    def test_waveform_to_spectogram_shape(self):
+        waveform = np.random.normal(size=(2**14,))
+        spectogram = spectral.waveform_2_spectogram(waveform, frame_length=512, frame_step=128)
+        
+        self.assertEqual((128, 256, 2), spectogram.shape)
+        
+    def test_waveform_to_magnitude_shape(self):
+        waveform = np.random.normal(size=(2**14,))
+        magnitude = spectral.waveform_2_magnitude(waveform, frame_length=512, frame_step=128)
+        
+        self.assertEqual((128, 256), magnitude.shape)
+        
+    def test_waveform_to_spectogram_return(self):
+        waveform = np.random.normal(size=(2**14,))
+        spectogram = spectral.waveform_2_spectogram(waveform, frame_length=512, frame_step=128)
+        waveform_hat = spectral.spectogram_2_waveform(spectogram, frame_length=512, frame_step=128)
+        
+        # Account for extra samples from reverse transform
+        waveform_hat = waveform[0:len(waveform)]
+        
+        self.assertAllClose(waveform, waveform_hat)
+        
+    def test_waveform_to_magnitude_return(self):
+        waveform = np.random.normal(size=(2**14,))
+        spectogram = spectral.waveform_2_magnitude(waveform, frame_length=512, frame_step=128)
+        waveform_hat = spectral.magnitude_2_waveform(spectogram, frame_length=512, frame_step=128)
+        
+        # Account for extra samples from reverse transform
+        waveform_hat = waveform[0:len(waveform)]
+        
+        self.assertAllClose(waveform, waveform_hat)
+        
 
 
 if __name__ == '__main__':
