5c2e098b9ca0e7cce3d335bb2b3cacdc7931c8ff
Michael Chinen <mchinen@google.com>
2020-07-20 11:26:48 -0700

    tests: fix indentation, remove py2 imports.

--- utils/layers_test.py
+++ utils/layers_test.py
@@ -1,10 +1,6 @@
 # Lint as: python3
 """Tests for spectral."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import tensorflow as tf
 import numpy as np
 
@@ -13,13 +9,13 @@ import layers
 
 class LayersTest(tf.test.TestCase):
 
-  def test_conv_transpose_shape(self):
-      inputs = np.random.normal(size=(10, 5, 2))
-      conv_transpose = layers.Conv1DTranspose(filters=2,
-                                              kernel_size=1,
-                                              strides=1)
-      outputs = conv_transpose(inputs)
-      self.assertShapeEqual(inputs, outputs)
+    def test_conv_transpose_shape(self):
+        inputs = np.random.normal(size=(10, 5, 2))
+        conv_transpose = layers.Conv1DTranspose(filters=2,
+                                                kernel_size=1,
+                                                strides=1)
+        outputs = conv_transpose(inputs)
+        self.assertShapeEqual(inputs, outputs)
 
 
 if __name__ == '__main__':
--- utils/spectral_test.py
+++ utils/spectral_test.py
@@ -1,10 +1,6 @@
 # Lint as: python3
 """Tests for spectral."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import tensorflow as tf
 
 import spectral
@@ -12,9 +8,9 @@ import spectral
 
 class SpectralTest(tf.test.TestCase):
 
-  def test_give_me_a_name(self):
-    pass
+    def test_give_me_a_name(self):
+        pass
 
 
 if __name__ == '__main__':
-  googletest.main()
+    googletest.main()
