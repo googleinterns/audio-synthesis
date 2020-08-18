d19c519792fc1035488f28fc45c110d33501922b
Daniel Braithwaite <dbraithwaite@google.com>
2020-08-18 06:01:18 +0000

    Remove Refernces to MAESTRO in STFT GAN Scripts

--- train_stft_spec_gan.py
+++ train_stft_spec_gan.py
@@ -12,19 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Training Script for STFTWaveGAN on MAESTRO.
+"""Training Script for STFTSpecGAN on a waveform dataset.
 
 Follows the same setup as SpecPhaseGAN, but
 generates STFTs instead of Magnitude and Instantaneous
-Frequency. Additionally, a Waveform critic is added
+Frequency. Additionally, a magnitude spectrum critic is added
 """
 
 import os
 import tensorflow as tf
-from audio_synthesis.structures import spec_gan, wave_gan
+from audio_synthesis.structures import spec_gan
 from audio_synthesis.models import wgan
-from audio_synthesis.datasets import maestro_dataset
-from audio_synthesis.utils import maestro_save_helper as save_helper
+from audio_synthesis.datasets import waveform_dataset
+from audio_synthesis.utils import waveform_save_helper as save_helper
 from audio_synthesis.utils import spectral
 
 # Setup Paramaters
@@ -37,45 +37,45 @@ FFT_FRAME_LENGTH = 512
 FFT_FRAME_STEP = 128
 Z_IN_SHAPE = [4, 8, 1024]
 SPECTOGRAM_IMAGE_SHAPE = [-1, 128, 256, 2]
+MAGNITUDE_IMAGE_SHAPE = [-1, 128, 256, 1]
 SIGNAL_LENGTH = 2**14
 WAVEFORM_SHAPE = [-1, SIGNAL_LENGTH, 1]
 CRITIC_WEIGHTINGS = [1.0, 1.0/1000.0]
-CHECKPOINT_DIR = '_results/representation_study/STFTWaveGAN_HR/training_checkpoints/'
-RESULT_DIR = '_results/representation_study/STFTWaveGAN_HR/audio/'
-MAESTRO_PATH = 'data/MAESTRO_6h.npz'
+CHECKPOINT_DIR = '_results/representation_study/STFTSpecGAN_HR/training_checkpoints/'
+RESULT_DIR = '_results/representation_study/STFTSpecGAN_HR/audio/'
+DATASET_PATH = 'data/MAESTRO_6h.npz'
 
 def _get_discriminator_input_representations(stft_in):
-    """Computes the input representations for the STFTWaveGAN discriminator models,
+    """Computes the input representations for the STFTSpecGAN discriminator models,
     returning the input waveform and coresponding spectogram representations
 
     Args:
         x_in: A batch of stft with shape (-1, time, frequency_dims).
 
     Returns:
-        A tuple containing the waveform and spectogram representaions of
+        A tuple containing the stft and log magnitude spectrum representaions of
         x_in.
     """
+    
+    real = stft_in[:, :, :, 0]
+    imag = stft_in[:, :, :, 1]
+    magnitude = tf.sqrt(tf.square(real) + tf.square(imag))
+    magnitude = tf.math.log(magnitude + 1e-6)
 
-    waveform = spectral.stft_2_waveform(
-        stft_in, frame_length=FFT_FRAME_LENGTH, frame_step=FFT_FRAME_STEP
-    )
-    waveform = tf.squeeze(waveform)
-    waveform = waveform[:, 0:SIGNAL_LENGTH]
-
-    return (stft_in, waveform)
+    return (stft_in, magnitude)
 
 
 def main():
-    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    os.environ['CUDA_VISIBLE_DEVICES'] = ''
     print('Num GPUs Available: ', len(tf.config.experimental.list_physical_devices('GPU')))
 
-    raw_maestro = maestro_dataset.get_maestro_stft_dataset(
-        MAESTRO_PATH, frame_length=FFT_FRAME_LENGTH, frame_step=FFT_FRAME_STEP
+    raw_dataset = waveform_dataset.get_stft_dataset(
+        DATASET_PATH, frame_length=FFT_FRAME_LENGTH, frame_step=FFT_FRAME_STEP
     )
 
     generator = spec_gan.Generator(channels=2, in_shape=Z_IN_SHAPE)
-    discriminator = spec_gan.Discriminator(input_shape=SPECTOGRAM_IMAGE_SHAPE, weighting=CRITIC_WEIGHTINGS[1])
-    waveform_discriminator = wave_gan.Discriminator(input_shape=WAVEFORM_SHAPE, weighting=CRITIC_WEIGHTINGS[0])
+    discriminator = spec_gan.Discriminator(input_shape=SPECTOGRAM_IMAGE_SHAPE)
+    magnitude_discriminator = spec_gan.Discriminator(input_shape=MAGNITUDE_IMAGE_SHAPE)
 
     generator_optimizer = tf.keras.optimizers.Adam(1e-4, beta_1=0.5, beta_2=0.9)
     discriminator_optimizer = tf.keras.optimizers.Adam(1e-4, beta_1=0.5, beta_2=0.9)
@@ -90,14 +90,14 @@ def main():
             epoch, real, generated, SAMPLING_RATE, RESULT_DIR, get_waveform
         )
 
-    stft_wave_gan_model = wgan.WGAN(
-        raw_maestro, generator, [discriminator, waveform_discriminator], Z_DIM,
+    stft_spec_gan_model = wgan.WGAN(
+        raw_dataset, generator, [discriminator, magnitude_discriminator], Z_DIM,
         generator_optimizer, discriminator_optimizer, discriminator_training_ratio=D_UPDATES_PER_G,
         batch_size=BATCH_SIZE, epochs=EPOCHS, checkpoint_dir=CHECKPOINT_DIR, fn_save_examples=save_examples,
         fn_get_discriminator_input_representations=_get_discriminator_input_representations
     )
 
-    stft_wave_gan_model.train()
+    stft_spec_gan_model.train()
 
 if __name__ == '__main__':
     main()
