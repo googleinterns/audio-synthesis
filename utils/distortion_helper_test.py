2cc2d67801d0f97f3ce50d51b4de7abb89732247
Daniel Braithwaite <dbraithwaite@google.com>
2020-09-01 20:50:16 +0000

    Add Distortion Helper Utility

---
+++ utils/distortion_helper_test.py
@@ -0,0 +1,53 @@
+# Lint as: python3
+
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for distortion helpers."""
+
+import tensorflow as tf
+import numpy as np
+import os
+
+import distortion_helper
+
+class SpectralTest(tf.test.TestCase):
+
+    def test_add_noise_at_snr_channel_shape(self):
+        representation = np.random.normal(size=(128, 256, 1)).astype(np.float32)
+        distorted = distortion_helper.add_noise_at_snr_channel(representation, 0)
+        
+        self.assertEqual(representation.shape, distorted.shape)
+        
+    def test_distort_one_channel_representation_shape(self):
+        representation = np.random.normal(size=(128, 256, 1)).astype(np.float32)
+        distorted = distortion_helper.distort_one_channel_representation(
+            representation, 0, 2
+        )
+        
+        self.assertEqual(representation.shape, distorted.shape)
+        
+    def test_distort_multiple_channel_representation_shape(self):
+        representation = np.random.normal(size=(128, 256, 3)).astype(np.float32)
+        distorted = distortion_helper.distort_multiple_channel_representation(
+            representation, 0, 2
+        )
+        
+        self.assertEqual((representation.shape[-1],) + representation.shape, distorted.shape)
+        
+
+
+if __name__ == '__main__':
+    os.environ["CUDA_VISIBLE_DEVICES"] = ''
+    tf.test.main()
